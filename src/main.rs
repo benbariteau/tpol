8631c7c43fc5d3c7af360b4621176c70976aecc3
Ben Bariteau <ben.bariteau@gmail.com>
2018-07-26 16:53:43 -0700

    create basic rustyline loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate rustyline;
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
+
 fn main() {
-    println!("Hello, world!");
+    let mut line_editor = Editor::<()>::new();
+    loop {
+        let line_result = line_editor.readline("> ");
+        match line_result {
+            Ok(line) => println!("{}", line),
+            Err(ReadlineError::Interrupted) => {
+                return;
+            },
+            Err(ReadlineError::Eof) => {
+                return;
+            },
+            Err(err) => {
+                eprintln!("{:?}", err);
+                return;
+            }
+        }
+    }
 }
