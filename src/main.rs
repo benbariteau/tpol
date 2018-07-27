94886dcebe211ceb610a3c07056be116912b329d
Ben Bariteau <ben.bariteau@gmail.com>
2018-07-26 17:10:00 -0700

    add basic support with hardcoded git

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
 extern crate rustyline;
 use rustyline::Editor;
 use rustyline::error::ReadlineError;
+use std::process::Command;
 
 fn main() {
     let mut line_editor = Editor::<()>::new();
     loop {
         let line_result = line_editor.readline("> ");
         match line_result {
-            Ok(line) => println!("{}", line),
+            Ok(line) => {
+                let mut process_result = Command::new("git")
+                    .args(line.split(" ").collect::<Vec<&str>>())
+                    .spawn();
+                match process_result {
+                    Ok(mut process) => {
+                        let _ = process.wait();
+                    },
+                    Err(err) => println!("error while trying to run command: {:?}", err),
+                }
+            },
             Err(ReadlineError::Interrupted) => {
                 return;
             },
