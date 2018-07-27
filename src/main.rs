7a98244f39bf58bc6b470ade9d6cbb91ebb8a70d
Ben Bariteau <ben.bariteau@gmail.com>
2018-07-27 15:16:54 -0700

    add support for custom prompt string commands

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate rustyline;
+extern crate serde;
+extern crate serde_json;
 use rustyline::Editor;
 use rustyline::error::ReadlineError;
 use rustyline::completion::Completer;
@@ -8,6 +10,7 @@ use std::process::Command;
 use std::env::home_dir;
 use std::fs::create_dir;
 use std::fs::File;
+use std::collections::HashMap;
 
 struct BashCompleter{
     command: String,
@@ -64,7 +67,7 @@ fn main() {
     let mut line_editor = Editor::<BashCompleter>::new();
     line_editor.set_completer(Some(BashCompleter{command: program.to_string()}));
 
-    let history_file_option = match home_dir() {
+    let (history_file_option, prompts_config_path_option) = match home_dir() {
         Some(home) => {
             let config_dir: PathBuf = [home, PathBuf::from(".tpol")].iter().collect();
             if !config_dir.exists() {
@@ -72,21 +75,29 @@ fn main() {
                 create_dir(&config_dir).unwrap();
             }
 
-            let history_dir: PathBuf = [config_dir, PathBuf::from("history")].iter().collect();
-            if !history_dir.exists() {
-                println!("creating history dir at {}", history_dir.to_str().unwrap());
-                create_dir(&history_dir).unwrap();
-            }
+            (
+                {
+                    let history_dir: PathBuf = [&config_dir, &PathBuf::from("history")].iter().collect();
+                    if !history_dir.exists() {
+                        println!("creating history dir at {}", history_dir.to_str().unwrap());
+                        create_dir(&history_dir).unwrap();
+                    }
 
-            let command_history: PathBuf = [history_dir, PathBuf::from(&program)].iter().collect();
-            if !command_history.exists() {
-                println!("creating history file for {} at {}", &program, command_history.to_str().unwrap());
-                let _ = File::create(&command_history).unwrap();
-            }
+                    let command_history: PathBuf = [history_dir, PathBuf::from(&program)].iter().collect();
+                    if !command_history.exists() {
+                        println!("creating history file for {} at {}", &program, command_history.to_str().unwrap());
+                        let _ = File::create(&command_history).unwrap();
+                    }
 
-            Some(command_history)
+                    Some(command_history)
+                },
+                {
+                    let prompts_config_path: PathBuf = [&config_dir, &PathBuf::from("prompts.json")].iter().collect();
+                    Some(prompts_config_path)
+                },
+            )
         }
-        None => None,
+        None => (None, None)
     };
 
     if let Some(ref history_file) = history_file_option.clone() {
@@ -97,8 +108,36 @@ fn main() {
         eprintln!("Warning: can't find home directory, no history will be saved");
     }
 
+    let program_to_prompt_string_command = match prompts_config_path_option {
+        Some(prompts_config_path) => {
+            File::open(prompts_config_path)
+                .map(|file| {
+                    let prompts_config: HashMap<String, String> = serde_json::from_reader(file)
+                        .unwrap_or(HashMap::new());
+                    prompts_config
+                })
+                .unwrap_or(HashMap::new())
+        }
+        None => HashMap::new(),
+    };
+
+    let prompt_string_command_option = program_to_prompt_string_command.get(&program);
+
     loop {
-        let line_result = line_editor.readline(format!(">{} ", program.clone()).as_str());
+        let prompt_string = match prompt_string_command_option.as_ref() {
+            Some(ref command) => {
+                match Command::new("bash").args(vec!["-lic", command]).output() {
+                    Ok(output) => String::from_utf8(output.stdout).unwrap(),
+                    Err(_) => {
+                        // TODO log error
+                        "".to_string()
+                    }
+                }
+            },
+            None => "".to_string(),
+        };
+
+        let line_result = line_editor.readline(format!("{}>{} ", prompt_string, &program).as_str());
         match line_result {
             Ok(line) => {
                 let mut process_result = Command::new(&program)
